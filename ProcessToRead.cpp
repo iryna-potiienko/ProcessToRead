244a7c2e7ea2855d782a9a79c6a81f77e11faacd
Iryna I. Potiienko <iryna.potiienko@gmail.com>
2021-06-05 21:05:57 +0300

    Add project files.

---
+++ ProcessToRead.cpp
@@ -0,0 +1,19 @@
+﻿// ProcessToRead.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma warning(disable: 4996)
+int main() {
+	char* message = NULL;
+	message = (char*)malloc(100);
+	memset((void*)message, 0, 100);
+	sprintf(message, "I'm here, please read me!");
+	printf("Process ID: %d\nMessage: %s\nAddress: %p\n", GetCurrentProcessId(), message, message);
+	getchar();
+	printf("Aftermath: Message: %s Address: %p\n", message, message);
+	getchar();
+	return 0;
+}
